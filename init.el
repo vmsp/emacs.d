5c82ad4d768a1840b3f9e931b83cc9faa8dca1f3
Vitor <vmsousapereira@gmail.com>
2023-01-20 18:51:40 +0000

    Let's try eshell

--- init.el
+++ init.el
@@ -190,7 +190,44 @@
   (exec-path-from-shell-initialize))
 
 (use-package eshell
-  :bind ("C-x m" . eshell))
+  :ensure nil
+  :bind ("C-x m" . eshell)
+  :config
+  (defun vsp/prompt-pwd (path max-len)
+    "Return a potentially trimmed-down version of the directory PATH, replacing
+parent directories with their initial characters to try to get the character
+length of PATH (sans directory slashes) down to MAX-LEN."
+    (let* ((components (split-string (abbreviate-file-name path) "/"))
+           (len (+ (1- (length components))
+                   (cl-reduce '+ components :key 'length)))
+           (str ""))
+      (while (and (> len max-len) (cdr components))
+        (setq str (concat str
+                          (cond ((= 0 (length (car components))) "/")
+                                ((= 1 (length (car components)))
+                                 (concat (car components) "/"))
+                                (t
+                                 (if (string= "."
+                                              (string (elt (car components) 0)))
+                                     (concat (substring (car components) 0 2)
+                                             "/")
+                                   (string (elt (car components) 0) ?/)))))
+              len (- len (1- (length (car components))))
+              components (cdr components)))
+      (concat str (cl-reduce (lambda (a b) (concat a "/" b)) components))))
+
+  (defun vsp/with-face (str &rest face-plist)
+    (propertize str 'face face-plist))
+
+  (defun vsp/eshell-prompt-function ()
+    (concat
+     (vsp/with-face
+      (vsp/prompt-pwd (eshell/pwd) 1) :foreground (doom-color 'green))
+     (vsp/with-face " ❱" :foreground (doom-color 'red))
+     (vsp/with-face " " :foreground (doom-color 'fg))))
+
+  (setq eshell-prompt-function 'vsp/eshell-prompt-function
+        eshell-highlight-prompt nil))
 
 (use-package project
   ;; Emacs' built-in projectile-ish mode. Default prefix is C-x p.
